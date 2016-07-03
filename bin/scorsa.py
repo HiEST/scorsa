d4eef994ac30344403393bddfacafbac8e268911
Jordà Polo <jorda.polo@bsc.es>
2016-07-03 22:41:10 +0200

    Add initial measurement of fragmentation

--- bin/scorsa.py
+++ bin/scorsa.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+from collections import defaultdict
+from itertools import groupby
+from operator import itemgetter
+
 def f2step(f, step, digits):
     s = f - (f % step)
     s = s + step if f % step != 0 else s
@@ -12,12 +16,12 @@ def rangef(start, stop, step, digits):
         yield round(r, digits)
         r += step
 
-def map_layout(layout):
+def map_layout(data):
     m = {}
     rack_id = 0
     drawer_id = 0
 
-    for (x,y), value in np.ndenumerate(layout):
+    for (x,y), value in np.ndenumerate(data):
         if y == 0:
             rack_id = 0
 
@@ -30,21 +34,40 @@ def map_layout(layout):
         if value in ["--", "|", "-1"]:
             continue
 
-        cpu_id = int(value)
-        m[cpu_id] = {}
-        m[cpu_id]["x"] = x
-        m[cpu_id]["y"] = y
-        m[cpu_id]["rid"] = rack_id
-        m[cpu_id]["did"] = drawer_id
+        cid = int(value)
+        m[cid] = {}
+        m[cid]["x"] = x
+        m[cid]["y"] = y
+        m[cid]["rid"] = rack_id
+        m[cid]["did"] = drawer_id
 
     return m
 
-def distance(procs, a, b):
+def distance(layout, a, b):
     d = 0
     if a != b:
 	d += 1
-    if procs[a]["did"] != procs[b]["did"]:
+    if layout[a]["did"] != layout[b]["did"]:
         d += 10
-    if procs[a]["rid"] != procs[b]["rid"]:
+    if layout[a]["rid"] != layout[b]["rid"]:
         d += 100
     return d
+
+def fragmentation(layout, subset):
+    f = 0.0
+    by_rack = defaultdict(list)
+
+    if len(subset) == 0:
+        return f
+
+    for cid in subset:
+        by_rack[layout[cid]['rid']].append(cid)
+
+    for rid, cids in by_rack.iteritems():
+        cids = sorted(cids)
+        fragments = []
+        for k, group in groupby(enumerate(cids), lambda (i, x): i - x):
+            fragments.append(len(map(itemgetter(1), group)))
+        f += 1 - (float(max(fragments))/sum(fragments))
+
+    return f / len(by_rack)
